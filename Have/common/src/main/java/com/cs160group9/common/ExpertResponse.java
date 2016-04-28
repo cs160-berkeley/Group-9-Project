1a440211699afef88d9aadde8365a210bfc6e126
Bill Cao <billcao@berkeley.edu>
2016-04-27 21:35:09 -0700

    Basic data structures, PatientRequest and ExpertResponse

---
+++ Have/common/src/main/java/com/cs160group9/common/ExpertResponse.java
@@ -0,0 +1,13 @@
+package com.cs160group9.common;
+
+import com.cs160group9.common.PatientRequest;
+
+public class ExpertResponse {
+
+    public PatientRequest patientRequest;
+    public boolean haveSTDSwipeResponse;
+    public String expertVoiceResponse;
+
+    public ExpertResponse() {}
+
+}
