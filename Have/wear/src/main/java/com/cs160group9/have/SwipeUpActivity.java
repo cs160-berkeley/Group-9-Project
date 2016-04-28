281734b239f622fef8826636c49c530c86ce52e6
Bill Cao <billcao@berkeley.edu>
2016-04-27 20:32:06 -0700

    Watch code readded

---
+++ Have/wear/src/main/java/com/cs160group9/have/SwipeUpActivity.java
@@ -0,0 +1,17 @@
+package com.cs160group9.have;
+
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+
+import cs160group9.com.have.R;
+
+public class SwipeUpActivity extends WearableActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_up);
+
+
+    }
+}
