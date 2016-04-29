95ad175f0cc9eb517b2485dbb95df62d25b0926a
Mario Guerrieri <mguerrieri@outlook.com>
2016-04-28 23:11:47 -0700

    Some work on onboard flow, switched to different wizard UI library

---
+++ Have/mobile/src/main/java/cs160group9/com/have/PatientOnboardActivity.java
@@ -0,0 +1,69 @@
+package cs160group9.com.have;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.redbooth.WelcomeCoordinatorLayout;
+
+public class PatientOnboardActivity extends AppCompatActivity {
+    private static final String TAG = "PatientOnboardActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_patient_onboard);
+
+        final WelcomeCoordinatorLayout welcomeCoordinator =
+                (WelcomeCoordinatorLayout) this.findViewById(R.id.welcomeCoordinator);
+        assert welcomeCoordinator != null;
+        welcomeCoordinator.setScrollingEnabled(false);
+        welcomeCoordinator.addPage(R.layout.patient_onboard_step_2);
+        welcomeCoordinator.addPage(R.layout.patient_onboard_step_1);
+
+        Button step1NextButton = (Button) this.findViewById(R.id.step_1_nextButton);
+        assert step1NextButton != null;
+        step1NextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                welcomeCoordinator.setCurrentPage(1, true);
+            }
+        });
+
+        Button step2NextButton = (Button) this.findViewById(R.id.step_2_nextButton);
+        assert step2NextButton != null;
+        step2NextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                welcomeCoordinator.setCurrentPage(2, true);
+            }
+        });
+        Button step2BackButton = (Button) this.findViewById(R.id.step_2_backButton);
+        assert step2BackButton != null;
+        step2BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                welcomeCoordinator.setCurrentPage(0, true);
+            }
+        });
+
+        EditText step1Field = (EditText) findViewById(R.id.step_1_field);
+        assert step1Field != null;
+        step1Field.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                    welcomeCoordinator.setCurrentPage(1, true);
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+    }
+}
--- Have/mobile/src/main/java/cs160group9/com/have/RoleChoiceActivity.java
+++ Have/mobile/src/main/java/cs160group9/com/have/RoleChoiceActivity.java
@@ -1,13 +1,35 @@
 package cs160group9.com.have;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RoleChoiceActivity extends AppCompatActivity {
+    private RoleChoiceActivity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_role_choice);
+        this.setContentView(R.layout.activity_role_choice);
+
+        Button patientButton = (Button) this.findViewById(R.id.patientButton);
+        assert patientButton != null;
+        patientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.startActivity(new Intent(activity, PatientOnboardActivity.class));
+            }
+        });
+
+        Button expertButton = (Button) this.findViewById(R.id.expertButton);
+        assert expertButton != null;
+        expertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.startActivity(new Intent(activity, ExpertOnboardActivity.class));
+            }
+        });
     }
 }
